4d373c988ae8646d2401fd664d6daaa7a77ad9c9
Christian Carver <christianjcarv@gmail.com>
2019-11-05 09:08:25 -0700

    first commit

---
+++ power.py
@@ -0,0 +1,11 @@
+
+otherP = 15
+scatP = int(input("Scatterometer's Ave. Power in W: "))
+radiP = int(input("Radiometer's Ave. Power in W: "))
+orbitT = int(input("Orbit Time in minutes: "))
+sunlightPercent = int(input("Time in Sunlight in percent %: "))
+solarPanelArea = int(input("Solar Panel Surface Area in cm^2: "))
+battsize = int(input("Battery size in Whr: "))
+maxPwr = int(input("Max Power Draw in W: "))
+
+print (maxPwr / 0.02)
