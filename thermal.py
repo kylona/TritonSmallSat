2fa5cbb94592b847040467db820e9d2c2dc24a45
kylona <>
2019-11-05 09:42:10 -0700

    Add start to thermal calculator

---
+++ thermal.py
@@ -0,0 +1,13 @@
+sunQPA.min = 1322
+sunQPA.max = 1414
+albedoQPA.min = 79
+albedoQPA.max = 707
+
+componantP = int(input("Power from Componants in Watts: "))
+orbitT = int(input("Orbit Time in minutes: "))
+sunlightPercent = int(input("Time in Sunlight in percent %: "))
+sunlightTime = orbitT * sunlightPercent /1000
+solarPanelArea = int(input("Solar Panel Surface Area in cm^2: "))
+otherArea = int(input("Other Surface Area in cm^2: "))
+
+print (maxPwr / 0.02)
