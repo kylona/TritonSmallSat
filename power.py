8fae63e0e74fb0b08e9156ec2f0b4af35a026311
Christian Carver <christianjcarv@gmail.com>
2019-11-05 09:39:59 -0700

    minimum solar area based on percent in sunlight

--- power.py
+++ power.py
@@ -1,11 +1,16 @@
 
 otherP = 15
+solarDensity = 0.02
 scatP = int(input("Scatterometer's Ave. Power in W: "))
 radiP = int(input("Radiometer's Ave. Power in W: "))
-orbitT = int(input("Orbit Time in minutes: "))
+orbitT = int(input("Orbit Time in minutes: ")) # 100
 sunlightPercent = int(input("Time in Sunlight in percent %: "))
 solarPanelArea = int(input("Solar Panel Surface Area in cm^2: "))
+batteryDensity = 100 # 100mAhr/cm^3
 battsize = int(input("Battery size in Whr: "))
 maxPwr = int(input("Max Power Draw in W: "))
 
-print (maxPwr / 0.02)
+temp = 15 + scatP * 0.07 + radiP * 0.93
+print('Average Power Usage ' + str(temp))
+area = temp / (sunlightPercent / 100) / solarDensity
+print('Minimum Solar Area ' + str(area))
