fe915a19a0e227057399bf1fc940198564ba1c09
Christian Carver <christianjcarv@gmail.com>
2019-11-07 09:46:20 -0700

    latest

--- power.py
+++ power.py
@@ -9,8 +9,14 @@ solarPanelArea = int(input("Solar Panel Surface Area in cm^2: "))
 batteryDensity = 100 # 100mAhr/cm^3
 battsize = int(input("Battery size in Whr: "))
 maxPwr = int(input("Max Power Draw in W: "))
+voltage = int(input("EPS voltage (usually between 2.2V - 18V): "))
 
-temp = 15 + scatP * 0.07 + radiP * 0.93
+temp = otherP + scatP * 0.07 + radiP * 0.93
+print('For 7% Scatterometer usage')
 print('Average Power Usage ' + str(temp))
 area = temp / (sunlightPercent / 100) / solarDensity
 print('Minimum Solar Area ' + str(area))
+
+
+percentage = ((solarPanelArea * solarDensity/100 * sunlightPercent) - otherP - radiP) / (scatP - radiP) * 100
+print('Scatterometer on time Percentage: ' + str(percentage))
