f6977e071b78ebfa58ee661ce3ff7b996f37484e
Kyle Storey <kyle.r.storey@gmail.com>
2020-02-12 08:23:06 -0700

    Compute Required SNR for Scaterrometry

--- antenna_analysis.py
+++ antenna_analysis.py
@@ -16,13 +16,29 @@ min_wavelength = 16.7 / (10**3)
 boltz = 1.38064852 / (10**23) #boltsman constant
 pi = 3.141592654
 eta = 120*pi #wave impedance of free space
+adc_enob = 8
 
 
 def kpc(snr, bandwidth, time):
   noise = math.sqrt( ((1 + (1/snr))*(1 + (1/snr))) + ((1/snr)*(1/snr)) )
-  root_bandwidth_time = math.sqrt(bandwidth * time)
+  root_bandwidth_time = np.sqrt(bandwidth * time)
   kpc = (1/root_bandwidth_time)*noise
   return kpc
+
+def computeRequiredSNR(kpc, bandwidth, time):
+  return (np.sqrt(2*bandwidth*kpc*kpc*time-1) + 1)/(bandwidth*kpc*kpc*time-1)
+
+
+def printSNRComponantRequirements(snr):
+  print("SYSTEM SNR: ", str(20*np.log10(snr)))
+  adc_snr = 6.02 * adc_enob * 1.76
+  print("ADC SNR: ", adc_snr)
+  adc_snr = 10**(adc_snr/20)
+  print("REMAINING SNR: ", 20*np.log10(1/(1/snr - 1/adc_snr)))
+
+print(20*np.log10(computeRequiredSNR(0.002, 250*(10**6),1/250)))
+printSNRComponantRequirements(computeRequiredSNR(0.002, 250*(10**6),1/250))
+  
 
 def stokes(vSig, hSig):
   Stokes = [];
@@ -33,6 +49,11 @@ def stokes(vSig, hSig):
   return Stokes
   
 
+# From Dr. Long page 295
+# T_0 = 290 degrees K = the physical temperature of the antenna
+# 120 < T_B = T_ML < 300
+#T_ML is main lobe temp T_SL is side lobe
+# T_A = 0.9 *0.8 * T_ML + 0.8*0.9*T_SL + 0.1*T_0
 def rbt(wavelength, bandwidth, vSig, hSig):
   Rbt = []
   Stokes = stokes(vSig, hSig)
@@ -42,7 +63,7 @@ def rbt(wavelength, bandwidth, vSig, hSig):
 
 def deltaT(T_A, T_rec, bandwidth, dwelltime):
   T_sys = T_A + T_rec;
-  return (T_sys) / np.sqrt(bandwidth*dwelltime)
+  return 2* ((T_sys) / np.sqrt(bandwidth*dwelltime)) #balanced Dicke from page 292
 
 def T_A(V_measured, a, b):
   #main_beam = the value of interest
@@ -89,4 +110,4 @@ ax.set_ylim(max_bandwidth, min_bandwidth)  # decreasing time
 #red_patch = mpatches.Patch(color='Orange', label='Hot Case')
 #blue_patch = mpatches.Patch(color='Blue', label='Cold Case')
 #plt.legend(handles=[red_patch, blue_patch])
-plt.show()
+#plt.show()
